0d1e0d7c12619982093827dec2384155925759fb
Nicoly Leal <155275519+Nicleal@users.noreply.github.com>
2025-11-14 09:12:51 -0300

    Create Exercício6.java

---
+++ ExVetores/Exercício6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ExVetores {
+
+ public static void main(String[] args) {
+  Scanner ler = new Scanner(System.in);
+  int i =0;
+  final int TAM = 11;
+  int[] a = new int [TAM];
+  int pot = 1;
+  
+  for(i=0;i<TAM;i++) {
+   a[i] = pot;
+   pot = pot*2;
+  }
+  System.out.println("Potências de 2 (de 2^0 a 2^10):");
+  for(i=0;i<TAM;i++) {
+   System.out.println("A[" + i + "] = " + a[i]);
+  }
+
+ }
+
+}
+ 
